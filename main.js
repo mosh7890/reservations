3b18951a13338517769e3c74481c234825299158
mosh7890 <mosh7890@gmail.com>
2017-05-24 11:56:51 +0300

    added input field

--- main.js
+++ main.js
@@ -3,21 +3,27 @@ var reservations = {
   'ted': { claimed: true }
 }
 
-var name = prompt('Please enter the name for your reservation').toLowerCase();
+document.getElementById("button1").onclick = function () {
+  var x = document.forms["form1"];
+  name = x.elements[0].value.toLowerCase();
 
-var claimReservation = function () {
-  if (name in reservations) {
-    if (reservations[name]['claimed']) {
-      alert("Your spot has been taken.")
+  var claimReservation = function () {
+    if (name in reservations) {
+      if (reservations[name]['claimed']) {
+        alert("Your spot has been taken.")
+      }
+      else {
+        reservations[name].claimed = true;
+        alert("Welcome!");
+      }
     }
     else {
-      alert("Welcome!");
+      reservations[name] = { claimed: true };
+      alert("A table has opened up, Welcome!");
     }
   }
-  else {
-    reservations[name] = { claimed: true };
-    alert("A table has opened up, Welcome!");
-  }
+
+  claimReservation();
 }
 
-claimReservation();
+
